707449a18a5090749adb6ae2c278819c37a2c1ea
Casey Murphy <cmurphy20@student.cvtc.edu>
2021-10-14 18:36:32 -0500

    added halloween theme funciton to index, quiz, videos

---
+++ js/halloween-quiz.js
@@ -0,0 +1,33 @@
+$(function() {
+
+    this.$spookyBtn = $('#halloween');
+
+    this.$spookyBtn.click(function(e) {
+
+        $('.container-fluid').css('background-color', "#FF7518");
+        $('#footer').css("background-color", "#FF7518");
+        $('h1.big-heading').css("color", "#000");
+        
+        $('h1.big-heading').text('Watch Out!');
+        $('#appName').css("color", "#000");
+        $(".fas").attr("class", "fas fa-ghost fa-7x"); 
+        $(".fas").css("color", "#FF5733");      
+
+        $('#halloween').fadeTo(1000, 0.0, function() {
+            $('#halloween').attr("class", "btn btn-light btn-lg");            
+            $('#halloween').fadeTo(500, 1);
+            $('#tooScary').attr('display', 'block');
+            $('#tooScary').fadeTo(500, 1);
+        })
+
+
+        $('#tooScary').click(function() {
+            location.reload(true);
+        });
+
+        
+        
+
+    })
+
+});
