8e185c35a856800170ac02690ad7e88070798535
SBandoli <77977329+Bertania@users.noreply.github.com>
2021-10-22 15:36:39 -0500

    Fixed merge errors and Halloween theme

--- js/halloween-quiz.js
+++ js/halloween-quiz.js
@@ -7,14 +7,12 @@ $(function() {
         $('.container-fluid').css('background-color', "#FF7518");
         $('#footer').css("background-color", "#FF7518");
         $('h1.big-heading').css("color", "#000");
+        $('#halloween').css("visibility", "hidden");
         
         $('h1.big-heading').text('Watch Out!');
         $('#appName').css("color", "#000");
         $(".fas").attr("class", "fas fa-ghost fa-7x"); 
-<<<<<<< HEAD
-        $(".fas").css("color", "#FF5733");      
-=======
-        $(".fas").css("color", "#FF5733"); 
+        $(".fas").css("color", "#FF5733");       
         
         $(".fa-ghost").mouseenter(function() {
             $(this).css("color", "#fff");
@@ -23,7 +21,6 @@ $(function() {
         $(".fa-ghost").mouseleave(function() {
             $(this).css("color", "#FF5733");
         });
->>>>>>> 2ced93640031c12e6c019e7a135a637755ac2fbe
 
         $('#halloween').fadeTo(1000, 0.0, function() {
             $('#halloween').attr("class", "btn btn-light btn-lg");            
@@ -42,8 +39,4 @@ $(function() {
 
     })
 
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 2ced93640031c12e6c019e7a135a637755ac2fbe
